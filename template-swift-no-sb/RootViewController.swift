e808ebab78407c894564b27bba97bb9c58212167
Glenn Adams <flgadams@gmail.com>
2022-08-18 20:15:39 -0400

    added namespace

--- template-swift-no-sb/RootViewController.swift
+++ template-swift-no-sb/RootViewController.swift
@@ -4,6 +4,10 @@
 //
 //  Created by Glenn Adams on 7/30/22.
 // A Swift project template for UIKit apps, no storyboards
+// dog is in a group, but we just specify to and it is found
+//
+// banana is in an asset folder "fruits"  ( r > in the right pane to create a folder, and turn on the namespace property
+
 
 import UIKit
 
@@ -13,7 +17,21 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .systemGray
         
+        let img = UIImage(named: "dog")
+        if img == nil {
+            print("nil image ")
+        }
+        let iv = UIImageView()
+        self.view.addSubview(iv)
+        iv.image = img
+        iv.frame = CGRect(x: 0, y: 50, width: 100, height: 100)
         
+        // fuits is a asset folder with "namespace" selected
+        let img2 = UIImage(named: "fruits/banana")
+        let imageView = UIImageView()
+        self.view.addSubview(imageView)
+        imageView.image = img2
+        imageView.frame = CGRect(x: 0, y: 200, width: 100, height: 100)
         
         
         
